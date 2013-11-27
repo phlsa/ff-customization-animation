7d20dd31656ac1ec867d58044342e252d97371b0
Philipp Sackl <philippsackl@gmail.com>
2013-11-27 17:28:08 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var grabX, grabY;
+
+$(document).ready(function() {
+	$('.button').on('mousedown mouseup', function(e) {
+		grabX = ( e.pageX - $('.button').offset().left );
+		grabY = ( e.pageY - $('.button').offset().top );
+		$(e.currentTarget).toggleClass('dragging');
+	});
+	$(document).on('mousemove', function(e) {
+		var btn = $('.button');
+		if ( btn.hasClass('dragging') ) {
+			var x = (e.pageX - grabX) + 'px';
+			var y = (e.pageY - grabY) + 'px';
+			$('.button').css({ left:x, top:y });
+		}
+	});
+});
